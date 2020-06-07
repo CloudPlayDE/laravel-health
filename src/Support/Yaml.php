212cace1cdd100af6a644d8940a5ded9f5ef72e8
Artem Stepin <stepin.artem@gmail.com>
2020-06-07 14:28:53 +0200

    fixed PHP 7.3 notices

--- src/Support/Yaml.php
+++ src/Support/Yaml.php
@@ -59,7 +59,7 @@ class Yaml
         preg_match_all('/{{(.*)}}/', $contents, $matches);
 
         foreach ($matches[0] as $key => $match) {
-            if (count($match)) {
+            if (is_countable($match) && count($match)) {
                 $contents = str_replace($matches[0][$key], $this->resolveVariable($matches[1][$key]), $contents);
             }
         }
@@ -94,7 +94,7 @@ class Yaml
     {
         preg_match_all('/(.*)\((.*)\)/', $string, $matches);
 
-        if (count($matches) && count($matches[0])) {
+        if (is_countable($matches) && count($matches) && is_countable($matches[0]) && count($matches[0])) {
             $function = $matches[1][0];
 
             return $function($this->removeQuotes($matches[2][0]));
